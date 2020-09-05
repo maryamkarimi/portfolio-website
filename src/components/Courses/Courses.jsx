e8b5e2e1b75f75bd911358bb69849a08d2fa92f5
Maryam Karimi <mkarimif@uwo.ca>
2020-09-04 20:53:41 -0400

    Made the courses page responsive

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -6,6 +6,7 @@ import Row from 'react-bootstrap/Row';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import Col from 'react-bootstrap/Col';
 import data from '../../data/courses.json';
 
 function Courses() {
@@ -13,41 +14,52 @@ function Courses() {
     <div id="courses">
       <Container id="courses-container">
         <Row id="courses-header">
-          <h2 id="courses-title">
-            List of Courses
-          </h2>
-          <h2>
-            <Link to="/">
-              <FontAwesomeIcon icon={faHome} />
-            </Link>
-          </h2>
+          <Col xs={{ offset: 1, span: 9 }}>
+            <h2>
+              List of Courses
+            </h2>
+          </Col>
+          <Col>
+            <h2>
+              <Link to="/">
+                <FontAwesomeIcon icon={faHome} />
+              </Link>
+            </h2>
+          </Col>
+
         </Row>
         <Row>
-          <Table id="courses-table" size="sm" responsive>
-            <thead>
-              <tr>
-                <th>Course Number</th>
-                <th>Course Name</th>
-                <th>Grade</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.courses.map((course) => (
+          <Col xs={{ offset: 1, span: 10 }}>
+            <Table id="courses-table">
+              <thead>
                 <tr>
-                  {course.school === 'uwo'
-                  && (
-                  <td>
-                    <a href={`https://www.westerncalendar.uwo.ca/Courses.cfm?CourseAcadCalendarID=${course.schoolId}`}>{course.id}</a>
-                  </td>
-                  )}
-                  {course.school !== 'uwo'
-                  && <td>{course.id}</td>}
-                  <td>{course.name}</td>
-                  <td>{course.grade}</td>
+                  <th><h6 className="font-weight-bold">Course Number</h6></th>
+                  <th><h6 className="font-weight-bold">Course Name</h6></th>
+                  <th><h6 className="font-weight-bold">Grade</h6></th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
+              </thead>
+              <tbody>
+                {data.courses.map((course) => (
+                  <tr>
+                    {course.school === 'uwo'
+                    && (
+                    <td>
+                      <a href={`https://www.westerncalendar.uwo.ca/Courses.cfm?CourseAcadCalendarID=${course.schoolId}`}>
+                        <h6>
+                          {course.id}
+                        </h6>
+                      </a>
+                    </td>
+                    )}
+                    {course.school !== 'uwo'
+                    && <td><h6>{course.id}</h6></td>}
+                    <td><h6>{course.name}</h6></td>
+                    <td><h6>{course.grade}</h6></td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
         </Row>
       </Container>
     </div>
