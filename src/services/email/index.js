879b478b7b301f7950ce7989a18eb99e6c6e0ce1
Maryam Karimi <mkarimif@uwo.ca>
2020-10-26 14:38:08 -0400

    resolved eslint warnings

--- src/services/email/index.js
+++ src/services/email/index.js
@@ -1,14 +1,14 @@
 import { getApiUrl, handleError } from '../index';
 
 export const sendEmail = (body) => {
-  const headers = new Headers({
-    'Content-Type': 'application/json',
-  });
-  
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+    });
+
     return fetch(`${getApiUrl()}`,
-    {
-      method: 'POST',
-      headers,
-      body: JSON.stringify({ ...body }),
-    }).then(handleError);
+        {
+            method: 'POST',
+            headers,
+            body: JSON.stringify({...body}),
+        }).then(handleError);
 };
